bfed04b20f26ea9eb9ba0db613e9c51b742debbd
braxey <bradleyjohnson0009@gmail.com>
2025-04-24 22:25:41 -0400

    fix tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,23 +2,23 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   test "should not save user with invalid email" do
-    user = User.new(email_address: "invalid", password: "password")
+    user = User.new(email_address: "invalid", password: "password", first_name: "Bradley", last_name: "Johnson")
     assert_not user.save
   end
 
   test "should not save user with existing email" do
-    User.create(email_address: "test@gillyware.com", password: "password")
-    user = User.new(email_address: "test@gillyware.com", password: "another_password")
+    user_one = users(:one)
+    user = User.new(email_address: user_one.email_address, password: "another_password", first_name: "Bradley", last_name: "Johnson")
     assert_not user.save
   end
 
   test "should not save user with password shorter than 8 characters" do
-    user = User.new(email_address: "test@gillyware.com", password: "short")
+    user = User.new(email_address: "test@gillyware.com", password: "short", first_name: "Bradley", last_name: "Johnson")
     assert_not user.save
   end
 
   test "should save valid user" do
-    user = User.new(email_address: "test@gillyware.com", password: "password")
+    user = User.new(email_address: "test@gillyware.com", password: "password", first_name: "Bradley", last_name: "Johnson")
     assert user.save
   end
 end
