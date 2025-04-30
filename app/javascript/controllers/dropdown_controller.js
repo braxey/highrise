41acb00da8f98d147081c3b06021126457809374
braxey <bradleyjohnson0009@gmail.com>
2025-04-29 20:52:52 -0400

    set up js formatter

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,33 +1,33 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input", "selected", "menu"];
+  static targets = ["input", "selected", "menu"]
 
   connect() {
-    document.addEventListener("click", this.handleClickOutside.bind(this));
+    document.addEventListener("click", this.handleClickOutside.bind(this))
   }
 
   disconnect() {
-    document.removeEventListener("click", this.handleClickOutside.bind(this));
+    document.removeEventListener("click", this.handleClickOutside.bind(this))
   }
 
   toggle(event) {
-    event.preventDefault();
-    this.menuTarget.classList.toggle("hidden");
+    event.preventDefault()
+    this.menuTarget.classList.toggle("hidden")
   }
 
   select(event) {
-    event.preventDefault();
-    const value = event.currentTarget.dataset.value;
-    const label = event.currentTarget.dataset.label;
-    this.inputTarget.value = value;
-    this.selectedTarget.textContent = label;
-    this.menuTarget.classList.add("hidden");
+    event.preventDefault()
+    const value = event.currentTarget.dataset.value
+    const label = event.currentTarget.dataset.label
+    this.inputTarget.value = value
+    this.selectedTarget.textContent = label
+    this.menuTarget.classList.add("hidden")
   }
 
   handleClickOutside(event) {
     if (!this.element.contains(event.target)) {
-      this.menuTarget.classList.add("hidden");
+      this.menuTarget.classList.add("hidden")
     }
   }
 }
